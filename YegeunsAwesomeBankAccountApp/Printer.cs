ee852cd0096aad5b7d86662bab661a1d59c33409
ukm0613 <ukm0613@gmail.com>
2016-07-29 11:03:10 +0100

    Printer
    Updated the interface on printer so you can interact with is just on
    printer though

--- YegeunsAwesomeBankAccountApp/Printer.cs
+++ YegeunsAwesomeBankAccountApp/Printer.cs
@@ -11,11 +11,47 @@ namespace YegeunsAwesomeBankAccountApp
         public void PrintBalance(BankAccount AccountBalance)
         {
             double Balance = AccountBalance.GetBalance();
+
             Console.WriteLine("You have " + Balance + " in you bank account");
-   
-         
-                      
-            
+
+      
+            Console.Write("Press w to withdraw d to depsit and c to check balance ");
+
+            if (w)
+            {
+                Console.Write("You want to withdraw. How much would you like to withdraw?");
+
+                Console.Read();
+
+                int t;
+
+                t = int.Parse(Console.ReadLine());
+            }
+            if (d) 
+            {
+                Console.Write("You want to deposit. How much would you like to deposit?");
+
+
+                Console.Read();
+
+                int p;
+
+                p = int.Parse(Console.ReadLine());
+
+                else
+
+                Console.Write("You don't have enought money on this account");
+
+            }
+            if (c)
+            {
+                string c;
+
+                Console.Write(" Here is you balance " + AccountBalance);
+
+                
+           }                    
+ 
         }
  
  
