1efa98500447ff1daa7a774a0d8150b4252e3992
ukm0613 <ukm0613@gmail.com>
2016-08-02 09:46:58 +0100

    added } at the end of the printer

--- YegeunsAwesomeBankAccountApp/Program.cs
+++ YegeunsAwesomeBankAccountApp/Program.cs
@@ -10,24 +10,6 @@ namespace YegeunsAwesomeBankAccountApp
     {
         static void Main(string[] args)
         {
-            /**
-             * Here is where the method will be called that print to the console
-             * The objective here is to INSTANTIATE a Printer and a BankAccount
-             * and call the various methods to a achieve the following:
-             * 
-             * 1) Print the amount of money in the account
-             * 2) Add some money and print the new amount
-             * 3) Subtract some money and print the new amount
-             * 4) TRY to subtract some more, but fail (Operation, not program) because not enough to subtract)
-             */
-
-            /**
-             * But I don't want to see anywhere in THIS file the following:
-             * 
-             * Console.WriteLine("......");
-             */
-
-            // This instantiaes the printer and bank account
             Printer ThePrinter = new Printer();
             BankAccount BankBalance = new BankAccount();
 
@@ -46,14 +28,7 @@ namespace YegeunsAwesomeBankAccountApp
             BankBalance.Withdraw(WithdrawMoney);
 
             ThePrinter.PrintBalance(BankBalance);
-            
-           
 
-            
-
-
-
-
-        }
+        } while (escape.key != Consolekey.Escape);
     }
 }
